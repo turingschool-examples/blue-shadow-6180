eb9f85658f0a63ed96cf94e336d63ab84eed1045
Connor Richmond <richmond.connor@gmail.com>
2023-08-08 12:33:32 -0400

    explain member in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  # member defines routes for a single instance of the doctors resource so I can nest instead of doing:
+  # delete 'doctors/:doctor_id/patients/:patient_id/appointments', to: 'doctors#destroy'
   resources :doctors, only: [:show] do
     member do
       delete 'patients/:patient_id', to: 'doctors#destroy'
