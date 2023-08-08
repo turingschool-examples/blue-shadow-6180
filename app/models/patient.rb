af6a1236ae142b86974b53d5a0cb3d0ace51c47b
Connor Richmond <richmond.connor@gmail.com>
2023-08-08 12:12:42 -0400

    patient model test

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,7 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
+
+  scope :adults, -> { where("age > ?", 18) }
+  scope :alphabetical_order, -> { order(:name) }
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "scope :adults" do
+    it "includes adult patients (age > 18)" do
+      @patient1 = Patient.create(name: 'Katie Bryce', age: 24)
+      @patient2 = Patient.create(name: 'Denny Duquette', age: 39)
+      @patient3 = Patient.create(name: 'Zola Shepherd', age: 2)
+
+      expect(Patient.adults).to include(@patient1, @patient2)
+      expect(Patient.adults).not_to include(@patient3)
+    end
+  end
+
+  describe "scope :alphabetical_order" do
+    it "orders patients in ascending alphabetical order" do
+      @patient1 = Patient.create(name: 'Katie Bryce', age: 24)
+      @patient2 = Patient.create(name: 'Denny Duquette', age: 39)
+      @patient3 = Patient.create(name: 'Zola Shepherd', age: 2)
+
+      patients = Patient.alphabetical_order
+
+      expect(patients).to eq([@patient2, @patient1, @patient3])
+    end
+  end
 end
